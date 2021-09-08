83d2f1964428f5c292e054ae6e42252efecd70ff
AlexKremerRus <nekhorosheff.a.v@yandex.ru>
2021-09-08 15:27:06 +0300

    confirm for pytest with default_2

--- Selenium_course/conftest.py
+++ Selenium_course/conftest.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--user_language', action='store', default="en",
+    parser.addoption('--languages', action='store', default=None,
                      help="Choose language: ru,en")
 
 
@@ -14,18 +14,18 @@ def pytest_addoption(parser):
 def browser(request):
 
     browser_name = request.config.getoption("browser_name")
-    user_language = request.config.getoption("user_language")
+    user_language = request.config.getoption("languages")
 
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
         options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        options.add_experimental_option('prefs', {'intl.accept_languages': languages})
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
         fp = webdriver.FirefoxProfile()
-        fp.set_preference("intl.accept_languages", user_language)
+        fp.set_preference("intl.accept_languages", languages)
         browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
